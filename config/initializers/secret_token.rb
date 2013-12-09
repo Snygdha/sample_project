7519c2d993ce9d44ef6a87f90d80304e6d96ec39
Snigdha Mudunuri <csnigdham@gmail.com>
2013-12-09 14:29:05 -0700

    Created Cars Scaffold-test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleProject::Application.config.secret_key_base = '8c3b46a86061040383acc185affde87086a223fbc45b167d559e1e8a82ec18591e17017ea82ac41a4371bedce4f05a5c6dc2f4eb9d64b6ae1d0f598b1daf5af0'
